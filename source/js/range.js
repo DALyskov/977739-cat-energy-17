21a37bcb8c8ba46fbabfdcefa19e06635e7836a1
DALyskov <DALyskov@gmail.com>
2019-08-26 19:18:50 +0300

    Доделал все. Далее буду чистить example.scss

--- source/js/range.js
+++ source/js/range.js
@@ -1,94 +1,109 @@
 var range = document.querySelector(".example__range");
-var progress = document.querySelector(".example__progress");
-// var progress_ie11 = document.querySelector(".example__progress");
+var progress = range.querySelector(".example__progress");
 var img_before = document.querySelector(".example__img--before");
 var img_after = document.querySelector(".example__img--after");
-var docWidth = document.body.clientWidth;
-var tabletWidth = 768;
+var btn_before = range.querySelector(".example__btn--before");
+var btn_after = range.querySelector(".example__btn--after");
+var doc_width = document.body.clientWidth;
+var tablet_width = 768;
+var desktop_width = 1300;
 
-progress.addEventListener("input", function () {
-  img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 3) + "px , auto, auto)";
-  img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value) * 3) + "px)";
-  // console.log(docWidth);
-// // //   // img_before.style.width = (100 - progress.value) + "%";
-// // //   console.log(progress.value);
-// // //   // console.log(img_before.style.clip);
-// // //   // console.log(img_after.style.clip);
-});
+if (doc_width < tablet_width) {
+  window.addEventListener("resize", function () {
+    progress.value = 50;
+    img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 3) + "px , auto, auto)";
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value) * 3) + "px)";
+  });
 
-function progress_ie() {
-  var progress_ie11 = document.querySelector(".example__progress").value;
-  img_before.style.clip = "rect(auto, " + ((100 - progress_ie11) * 3) + "px , auto, auto)";
-  img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress_ie11) * 3) + "px)";
-}
+  btn_before.addEventListener("click", function () {
+    progress.value = 0;
+    img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 3) + "px , auto, auto)";
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value) * 3) + "px)";
+  });
 
-if (docWidth >= tabletWidth) {
+  btn_after.addEventListener("click", function () {
+    progress.value = 100;
+    img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 3) + "px , auto, auto)";
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value) * 3) + "px)";
+  });
+
   progress.addEventListener("input", function () {
-    // img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 6.9) + "px , auto, auto)";
-    // img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value) * 6.9) + "px)";
-    img_before.style.clip = "rect(auto, " + (((100 - progress.value) * 6.9) - 7) + "px , auto, auto)"; /*для сдвинутой фотографии*/
-    img_after.style.clip = "rect(auto, auto, auto, " + (((100 - progress.value) * 6.9) - 7) + "px)"; /*для сдвинутой фотографии*/
+    img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 3 + 1) + "px , auto, auto)";
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value) * 3 + 1) + "px)";
+  });
+
+  function progress_ie() {
+    var progress_ie11 = document.querySelector(".example__progress").value;
+    img_before.style.clip = "rect(auto, " + ((100 - progress_ie11) * 3) + "px , auto, auto)";
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress_ie11) * 3) + "px)";
+  }
+};
+
+if (doc_width >= tablet_width) {
+  window.addEventListener("resize", function () {
+    progress.value = 50;
+    img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 6.9 - 7) + "px , auto, auto)";
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value) * 6.9 - 7) + "px)";
+  });
+
+  btn_before.addEventListener("click", function () {
+    progress.value = 0;
+    img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 6.9 - 7) + "px , auto, auto)";
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value) * 6.9 - 7) + "px)";
     console.log(progress.value);
     console.log(img_before.style.clip);
     console.log(img_after.style.clip);
   });
+
+  btn_after.addEventListener("click", function () {
+    progress.value = 100;
+    img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 6.9 - 7) + "px , auto, auto)";
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value) * 6.9 - 7) + "px)";
+  });
+
+  progress.addEventListener("input", function () {
+    img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 6.9 - 7) + "px , auto, auto)"; /*для сдвинутой фотографии*/
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value) * 6.9 - 7) + "px)"; /*для сдвинутой фотографии*/
+  });
+
   function progress_ie() {
     var progress_ie11 = document.querySelector(".example__progress").value;
-    img_before.style.clip = "rect(auto, " + (((100 - progress_ie11) * 6.9) - 7) + "px , auto, auto)";
-    img_after.style.clip = "rect(auto, auto, auto, " + (((100 - progress_ie11) * 6.9) - 7) + "px)";
-    // img_before.style.clip = "rect(auto, " + ((100 - progress_ie11) * 6.9) + "px , auto, auto)";
-    // img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress_ie11) * 6.9) + "px)";
+    img_before.style.clip = "rect(auto, " + ((100 - progress_ie11) * 6.9 - 7) + "px , auto, auto)";
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress_ie11) * 6.9 - 7) + "px)";
   }
 };
 
+if (doc_width >= desktop_width) {
+  window.addEventListener("resize", function () {
+    progress.value = 50;
+    img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 6.9 + 20) + "px , auto, auto)";
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value) * 6.9 + 20) + "px)";
+  });
 
+  btn_before.addEventListener("click", function () {
+    progress.value = 0;
+    img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 6.9 + 20) + "px , auto, auto)";
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value) * 6.9 + 20) + "px)";
+  });
 
-// progress.addEventListener("input", function () {
-//   img_before.style.width = (100 - progress.value) + "%";
-//   img_after.style.width = progress.value + "%";
-// });
+  btn_after.addEventListener("click", function () {
+    progress.value = 100;
+    img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 6.9 + 20) + "px , auto, auto)";
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value) * 6.9 + 20) + "px)";
+  });
 
+  progress.addEventListener("input", function () {
+    img_before.style.clip = "rect(auto, " + ((100 - progress.value) * 6.9 + 20) + "px , auto, auto)"; /*для сдвинутой фотографии*/
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress.value * 6.9) + 20) + "px)"; /*для сдвинутой фотографии*/
+  });
 
+  function progress_ie() {
+    var progress_ie11 = document.querySelector(".example__progress").value;
+    img_before.style.clip = "rect(auto, " + ((100 - progress_ie11) * 6.9 + 20) + "px , auto, auto)";
+    img_after.style.clip = "rect(auto, auto, auto, " + ((100 - progress_ie11) * 6.9 + 20) + "px)";
+  }
+};
 
-//   if (progress) {
-//     var btnBefore = progress.querySelector(".demo__progress-button--before");
-//     var btnAfter = progress.querySelector(".demo__progress-button--after");
-
-//     btnBefore.addEventListener("click", function () {
-//       if (progress.classList.contains("demo__progress--after")) {
-//         progress.classList.remove("demo__progress--after");
-//       }
-//     })
-
-//     btnAfter.addEventListener("click", function () {
-//       if (!progress.classList.contains("demo__progress--after")) {
-//         progress.classList.add("demo__progress--after");
-//       }
-//     })
-
-//     if (docWidth >= tabletWidth) {
-//       var imgBefore = progress.querySelector(".demo__illustration--before");
-//       var imgAfter = progress.querySelector(".demo__illustration--after");
-//       var scale = progress.querySelector(".demo__progress-bar");
-//       var range = scale.querySelector(".demo__range");
-
-//       range.addEventListener("input", function() {
-//         imgBefore.style.width = (100 - range.value) + "%";
-//         imgAfter.style.width = range.value + "%";
-//       })
-
-//       btnBefore.addEventListener("click", function () {
-//         imgBefore.style.width = "100%";
-//         imgAfter.style.width = "0%";
-//         range.value = 0;
-//       })
-
-//       btnAfter.addEventListener("click", function () {
-//         imgBefore.style.width = "0%";
-//         imgAfter.style.width = "100%";
-//         range.value = 100;
-//       })
-//     }
-//   }
-
-// })();
+// console.log(progress.value);
+// console.log(img_before.style.clip);
+// console.log(img_after.style.clip);
